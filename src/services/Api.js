b12055c0b56505eaa374743e1f0d0f2c51178841
irynailina <irynailina@gmail.com>
2020-07-12 01:18:29 +0300

    swapi app done

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-export const fetchFilms = () => axios.get(`http://swapi.dev/api/films`)
-export const fetchFilmInfo = (id) => axios.get(`http://swapi.dev/api/films/${id}/`)
-export const fetchPeople = () => axios.get(`http://swapi.dev/api/people`)
-export const fetchCharacter = (id) => axios.get(`http://swapi.dev/api/people/${id}`) 
-export const fetchPlanet = (id) => axios.get(`http://swapi.dev/api/planets/${id}/`)
-export const fetchSpecies = (id) => axios.get(`http://swapi.dev/api/species/${id}/`)
-export const fetchStarship = (id) => axios.get(`http://swapi.dev/api/starships/${id}/`)
-export const fetchVehicle = (id) => axios.get(`http://swapi.dev/api/vehicles/${id}/`)
+export const fetchFilms = () => axios.get(`https://swapi.dev/api/films`)
+export const fetchFilmInfo = (id) => axios.get(`https://swapi.dev/api/films/${id}/`)
+export const fetchPeople = () => axios.get(`https://swapi.dev/api/people`)
+export const fetchCharacter = (id) => axios.get(`https://swapi.dev/api/people/${id}`) 
+export const fetchPlanet = (id) => axios.get(`https://swapi.dev/api/planets/${id}/`)
+export const fetchSpecies = (id) => axios.get(`https://swapi.dev/api/species/${id}/`)
+export const fetchStarship = (id) => axios.get(`https://swapi.dev/api/starships/${id}/`)
+export const fetchVehicle = (id) => axios.get(`https://swapi.dev/api/vehicles/${id}/`)
